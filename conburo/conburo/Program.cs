114981fbf3aaced7fb349b3f8b6a3890bbc6ac2e
Madiyar <nu_ofsn11@mail.ru>
2020-01-15 18:43:49 +0600

    Hello, world!

---
+++ conburo/conburo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conburo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
