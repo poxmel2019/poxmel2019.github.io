2f982073b39f28faa87ad0fc963d2248fe78f9e0
Madiyar <nu_ofsn11@mail.ru>
2020-01-29 16:46:44 +0600

    'Dicts'

--- conburo/conburo/Program.cs
+++ conburo/conburo/Program.cs
@@ -12,41 +12,7 @@ namespace conburo
     {
         static void Main(string[] args)
         {
-            /*
-            Dictionary<int, string> countries = new Dictionary<int, string>(5);
-            countries.Add(1, "Russia");
-            countries.Add(2, "Great Britain");
-            countries.Add(3, "USA");
-            countries.Add(4, "France");
-            countries.Add(5, "China");
-
-            //foreach (KeyValuePair<int,string> keyValue in countries)
-            //{
-            //    Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
-            //}
-
-            Dictionary<char, Person> people = new Dictionary<char, Person>();
-            people.Add('b', new Person() { Name = "Bill"});
-            people.Add('t', new Person() { Name = "Tom" });
-            people.Add('j', new Person() { Name = "John" });
-            people.Add('d', new Person() { Name = "Dilan" });
-            people['e'] = new Person() { Name = "Eric" };
-
-            foreach (KeyValuePair<char, Person> keyValue in people)
-            {
-                Console.WriteLine(keyValue.Key + " - " + keyValue.Value.Name);
-            }
-
-            foreach(char c in people.Keys)
-            {
-                Console.WriteLine(c);
-            }
-
-            foreach(Person n in people.Values)
-            {
-                Console.WriteLine(n.Name);
-            }
-            */
+           
             Dictionary<string, string> capitals = new Dictionary<string, string>
             {
                 { "France","Paris"},
