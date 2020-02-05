ae4136fd45bcbf4f911453f40fead570eaf1fd55
Madiyar <nu_ofsn11@mail.ru>
2020-02-05 14:50:27 +0600

    Linq itm

--- conburo/conburo/Program.cs
+++ conburo/conburo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace conburo
 {
@@ -8,17 +9,66 @@ namespace conburo
     {
         public string Name { get; set; }
     }
+    class User
+    {
+        protected string name;
+        protected int? age;
+
+        public string Name { get; set; }
+        public int? Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 0) age = 1;
+                else if (value > 120) age = 120;   
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            decimal num = 1234;
-            float num2 = 1234;
+            Dictionary<string,int?> dict = new Dictionary<string,int?>();
+            dict.Add("provider",123456);
+            dict.Add("service",null);
+            
+            foreach(KeyValuePair<string,int?> kv in dict)
+            {
+                Console.WriteLine(kv.Key + " - " + kv.Value);
+            }
 
+            List<User> users = new List<User>()
+            {
+                new User { Name = "Tom", Age = 23},
+                new User { Name = "Sam", Age = 43},
+                new User { Name = "Bill", Age = 35},
+                new User { Name = "Shiva", Age = null}
+            };
 
-            Console.WriteLine(num>num2);
-            Console.WriteLine(num.GetType());
+            bool res1 = users.Any(u => u.Age < 20 || u.Age == null);
+            if (res1)
 
+                Console.WriteLine("There are users with age less than 20");
+
+            else
+                Console.WriteLine("All users have age more than 20");
+
+            bool res2 = users.Any(u => u.Name.StartsWith("T"));
+            if (res2)
+            {
+                Console.WriteLine("There are users whose names starts with \"T\"");
+            }
+            else
+                Console.WriteLine("There are no users whose names starts with \"T\"");
+            Console.WriteLine(res1);
+            Console.WriteLine(res2);
+
+            string ass = null;
+            Console.WriteLine(ass);
             Console.WriteLine();
             Console.Read();
 
